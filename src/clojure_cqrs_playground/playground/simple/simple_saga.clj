04e79b5f6ac76f36435752770511c606b7eabc33
Jordi Llonch <llonch.jordi@gmail.com>
2020-10-18 10:14:20 +0200

    use just maps

--- src/clojure_cqrs_playground/playground/simple/simple_saga.clj
+++ src/clojure_cqrs_playground/playground/simple/simple_saga.clj
@@ -17,27 +17,30 @@
 
 
 ; saga handlers
-(defn run-simple-stuff [state event {:keys [some-dependency other-dependency]}]
-  {:cqrs/state  state
+(defn run-simple-stuff [{:keys [saga/state cqrs/event some-dependency other-dependency]}]
+  (printf event)
+  (printf some-dependency)
+  (printf other-dependency)
+  {:saga/state  (update state :stage (constantly :step-1))
    :cqrs/events [simple-stuff-done]})
 
-(defn run-more-stuff [state _ _]
-  {:cqrs/state  (update state :stage (constantly :step-2))
+(defn run-more-stuff [{:keys [saga/state]}]
+  {:saga/state  (update state :stage (constantly :step-2))
    :cqrs/events [more-stuff-done]})
 
-(defn run-extra-stuff-a [state _ {:keys [some-dependency]}]
-  {:cqrs/state  (update state :stage (constantly :step-3))
+(defn run-extra-stuff-a [{:keys [saga/state]}]
+  {:saga/state  (update state :stage (constantly :step-3))
    :cqrs/events [more-stuff-done]})
 
-(defn run-extra-stuff-b [state _ _]
-  {:cqrs/state  (update state :stage (constantly :cqrs/all-steps-done))
+(defn run-extra-stuff-b [{:keys [saga/state]}]
+  {:saga/state  (update state :stage (constantly :all-steps-done))
    :cqrs/events [all-stuff-done]})
 
 
 ; saga definition
 (def a-simple-saga
-  {:cqrs/initial-state {:stage :step-1}
-   :cqrs/handlers      [
+  {:saga/initial-state {:stage :step-0}
+   :saga/handlers      [
                         {:saga/on do-simple-stuff :saga/do run-simple-stuff}
                         {:saga/on simple-stuff-done :saga/do run-more-stuff}
                         {:saga/on more-stuff-done :saga/do run-extra-stuff-a :saga/when #(= (:stage %) :step-2)}
